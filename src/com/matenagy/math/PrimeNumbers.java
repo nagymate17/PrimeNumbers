077e2f02cf2c16966878362ee34bbac650d23f0f
nagymate17 <nagymate17.nm@gmail.com>
2017-08-31 14:44:43 +0200

    first try

---
+++ src/com/matenagy/math/PrimeNumbers.java
@@ -0,0 +1,5 @@
+package com.matenagy.math;
+
+public class PrimeNumbers {
+
+}
---
+++ test/com/matenagy/math/PrimeNumbersTest.java
@@ -0,0 +1,14 @@
+package com.matenagy.math;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimeNumbersTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
