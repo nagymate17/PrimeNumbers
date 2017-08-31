42dbe9c1c9a5350d3b71b50e71769551cebdc4c6
nagymate17 <nagymate17.nm@gmail.com>
2017-08-31 15:31:44 +0200

    Version 0.1 with a little testing

--- src/com/matenagy/math/PrimeNumbers.java
+++ src/com/matenagy/math/PrimeNumbers.java
@@ -1,5 +1,20 @@
 package com.matenagy.math;
 
-public class PrimeNumbers {
+import java.util.*;
+import java.util.stream.*;
 
+public class PrimeNumbers {
+	
+	
+	public static  List<Integer> prime( int num ){
+		if(num <= 0 || num > 10000) throw new IllegalArgumentException (" input is wrong...please give me az integer between 1 to 10000");
+		else return primeRec(IntStream.rangeClosed(2, num).boxed().collect(Collectors.toList()), new ArrayList<Integer>());
+	}
+	private static List<Integer> primeRec(List<Integer> in , List<Integer> out){
+		if(in.isEmpty()) return out;
+		int firstElement = in.get(0);
+		out.add(firstElement);
+		in = in.stream().filter( n -> n % firstElement  != 0 ).collect(Collectors.toList());
+		return primeRec(in,out);
+	}
 }
--- test/com/matenagy/math/PrimeNumbersTest.java
+++ test/com/matenagy/math/PrimeNumbersTest.java
@@ -2,13 +2,47 @@ package com.matenagy.math;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PrimeNumbersTest {
+	private static List<Integer> resultList = new ArrayList<Integer>();
+	private static int numbersOfTesting = 1;
+	
+	@Before
+	public void init(){
+		System.out.println("Testing ...");	
+	}
+	
+	@After
+	public void finallyy(){
+		System.out.format("End of testing %d \n",numbersOfTesting);
+		numbersOfTesting += 1;
+	}
+	
+	@Test
+	public void wrongArgument(){
+		try{
+			PrimeNumbers.prime(-1);
+		}catch(IllegalArgumentException e){
+			System.out.println("Illegal Argument Exception catched!");
+		} 	
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void firsElement() {
+		resultList = PrimeNumbers.prime(10000);
+		int result = resultList.get(0);
+		assert(2 == result);
 	}
+	
+	
+	
+	
+	
 
 }
